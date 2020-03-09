4ff132cba1dfc33217f39ae81fa640839a422d7a
francisco <frmauro8@hotmail.com>
2020-03-09 20:24:52 -0300

    Implementatio CRUD Movie

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,17 +15,33 @@ namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    {
+                        var connectionString = Configuration.GetConnectionString("MvcMovieContext");
+
+                        if (Environment.IsDevelopment())
+                        {
+                            options.UseSqlite(connectionString);
+                        }
+                        else
+                        {
+                            options.UseSqlServer(connectionString);
+                        }
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
